90ff77fa934f7b070e0b10441dfa54ff17af8ff1
hkim84 <112454703+hkim84@users.noreply.github.com>
2022-11-06 16:49:54 -0800

    added psuedocode for js

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// 1.Search for city
+// 2. present condition of weather current and futer
+// 3. add city to history
+// 4. current weather conditions for that city
+// 5. city name, the date, an icon representation of weather conditions, 
+// 6. temperature, the humidity, the wind speed, and the UV index
+// 7. I view the UV index
+// 8. presented with a color that indicates whether the conditions are favorable, moderate, or severe
+// 9. view future weather conditions for that city
+// 10. presented with a 5-day forecast that displays the date, 
+// 11. icon representation of weather conditions, the temperature, the wind speed, and the humidity
+// 12. click on a city in the search history
+// 13. presented with current and future conditions for that city
